b2ff5616172ca43f22d6aeb0af8f8e8813d58331
sahale <phil.zabolotny@gmail.com>
2018-07-07 19:39:18 +0300

    Done

---
+++ lesson_1/lesson_1_easy_1.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+# PEP-8
+
+# Задача-1: поработайте с переменными, создайте несколько,
+# выведите на экран, запросите от пользователя и сохраните в переменную, выведите на экран
+
+print("Hello world!\nThis is my first task on GeekBrain's python course!")
+name = input("Dear friend, type your name here: ")
+print("Glad to see you here,", name)
